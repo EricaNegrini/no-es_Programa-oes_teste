254949ddd7705dff034f9d9e7cb4d8f858ce803a
EricaNegrini <333ericarosa333@gmail.com>
2022-06-15 20:53:50 -0300

    segunda alteracao

--- Aula5.php
+++ Aula5.php
@@ -1,7 +1,7 @@
 <html>
 
     <head>
-<title> "incriçao do aluno" </title>
+<title> "incrição de cidade" </title>
 <meta charset="UTF-8">
 </head>
  
@@ -10,16 +10,38 @@
 
     <center>
 
-<h1>INCRIÇAO DA CIDADE</h1>
+<h1>INCRIÇAO</h1>
+
+<label> Nome: <label> <br>
+<input type="text" name="nome" require><br>
+<br>
+
+<label> Sobrenome: <label> <br>
+<input type="text" name="sobrenome" require><br>
+<br>
+
+<label> Idade: <label> <br>
+<input type="number" name="idade" require><br>
+<br>
+
+<label> Data: <label> <br>
+<input type="date" name="data" require><br>
+<br>
+
+<label> Peso: <label> <br>
+<input type="number" name="peso" require><br>
+<br>
+
+
 
 <label>
-    <b> Nome da cidade: </b>
+    <b>Cidade: </b>
     </label>
     <input type="text" name= "cidade" require> <br>
     <br>
     
     <label>
-        <b>Nome do Estado:</b>
+        <b>Estado:</b>
      </label>
      <input type="text" name="estado" requere> <br>
      <br>
@@ -27,7 +49,10 @@
     <button type= "submit">SALVAR</button>
 </center>
 
+<center>
 <?php
+print_r($_POST);
+
 class pessoa_obj{};
 $pessoa = new pessoa_obj();
 $pessoa->name="NICK";
@@ -57,55 +82,41 @@ $pessoa->cidade="Tarumã";
 $pessoa->estado="São Paulo";
 
 //mostrar em ordem
-print_r($pessoa);
-echo"<br><br>";
-print_r($pessoa2);
-echo"<br><br>";
-print_r($pessoa3);
-echo"<br><br>";
+// print_r($pessoa);
+// echo"<br><br>";
+// print_r($pessoa2);
+// echo"<br><br>";
+// print_r($pessoa3);
+// echo"<br><br>";
 
-
+ 
 $arrayPessoas = array();
-echo"<br>O meu array de pessoas: <br>";
-print_r($arrayPessoas);
-array_push($arrayPessoas, $pessoa);
-array_push($arrayPessoas, $pessoa2);
-array_push($arrayPessoas, $pessoa3);
-echo "<br><br>";
+    function inserirPessoaNaLista ($arrayPessoas, $item){
+    
+        $pessoa = new pessoa_obj();
+        $pessoa->name= $item['nome'];
+        $pessoa->sobrenome= $item['sobrenome'];
+        $pessoa->idade= $item['idade'];
+        $pessoa->date= $item ['data'];
+        $pessoa->peso=$item['peso'];
+        $pessoa->cidade=$item['cidade'];
+        $pessoa->estado=$item['estado'];
+
+        
+        array_push($arrayPessoas, $pessoa); 
+        return $arrayPessoas;
+    }
+
+//  echo"<br>O meu array de pessoas: <br>";
+//  print_r($arrayPessoas);
+ array_push($arrayPessoas, $pessoa);
+ array_push($arrayPessoas, $pessoa2);
+ array_push($arrayPessoas, $pessoa3);
+ echo "<br><br>";
 
 echo "<br><br><pre>";
-print_r($arrayPessoas);
+// print_r($arrayPessoas);
 echo "</pre><br><br>";
-
-//tabela
-echo "<table border='1'>";
-
-for ($i=0; $i < count($arrayPessoas); $i++) { 
-    if($arrayPessoas[$i] -> idade > 0 ){
-        echo"<tr>";
-        echo "<td>".$arrayPessoas[$i]->name."</td><br>";
-        echo "<td>".$arrayPessoas[$i]->sobrenome."</td><br>";
-        echo "<td>".$arrayPessoas[$i]->idade."</td><br>";
-        echo "<td>".$arrayPessoas[$i]->date."</td><br>";
-        echo "<td>".$arrayPessoas[$i]->peso."</td><br>";
-        echo "</tr>";
-    } 
-    echo "oi".$arrayPessoas[$i]->name."<br>"; 
-     echo "<br>";
-}
-
-//Mostrar do menor para o maior
-echo"</table>" ;
-echo "<br>";
-for ($i=count($arrayPessoas)-1; $i > -1; $i--) { 
-    echo "oi".$arrayPessoas[$i]->name."<br>"; 
-    
-}
-
-
-
-$variavel ="Tarumã";
-print_r ($_POST);
 
 if(isset ($_POST['cidade'])){
     if($_POST['cidade'] == ""){
@@ -113,41 +124,77 @@ if(isset ($_POST['cidade'])){
     }else if($_POST['estado'] == ""){
         echo "Esta vazio o campo Estado";
     }else{
+        $arrayPessoas = inserirPessoaNaLista($arrayPessoas, $_POST);
         echo "Seu cadastro foi um sucesso!!";
-    }
+   }
 
 }
 
+
+//tabela
+echo "<table border='1'>";
+
+for ($i=0; $i < count($arrayPessoas); $i++) { 
+    if($arrayPessoas[$i] -> idade > 0 ){
+        echo"<tr>";
+        echo "<td>".$arrayPessoas[$i]->name."</td>";
+        echo "<td>".$arrayPessoas[$i]->sobrenome."</td>";
+        echo "<td>".$arrayPessoas[$i]->idade."</td>";
+        echo "<td>".$arrayPessoas[$i]->date."</td>";
+        echo "<td>".$arrayPessoas[$i]->peso."</td>";
+        echo "</tr>";
+        echo"<br>";
+     }
+    //  echo "oi".$arrayPessoas[$i]->name."<br>"; 
+}
+echo"</table>" ;
+
+
+//Mostrar do menor para o maior
+// echo "<br>";
+// for ($i=count($arrayPessoas)-1; $i > -1; $i--) { 
+    // echo "oi".$arrayPessoas[$i]->name."<br>"; 
+    
+// }
+
+
+
+// $variavel ="Tarumã";
+ //print_r ($_POST);
+
+
+
 ?>
+</center>
 
 <center>
 
-<table border='1'> 
-   <tr>
-       <td>cidade:</td>
-       <td>estados:</td>
-   </tr>
-   <tr>
-      <td>Tarumã</td>
-      <td>São Paulo</td>
-   </tr>
-   <tr>
-       <td>Assis</td>
-       <td>São Paulo</td>
-    </tr>
-    <tr>
-       <td>Candido Mota</td>
-       <td>São Paulo</td>
-   </tr>
-   <tr>
-      <td>Londrina</td>
-      <td>Parana</td>
-   </tr>
-   <tr>
-       <td>Curitiba</td>
-       <td>Parana</td>
-    </tr>
-</table>
+<!-- <table border='1'>  -->
+   <!-- <tr> -->
+       <!-- <td>cidade:</td> -->
+       <!-- <td>estados:</td> -->
+   <!-- </tr> -->
+   <!-- <tr> -->
+      <!-- <td>Tarumã</td> -->
+      <!-- <td>São Paulo</td> -->
+   <!-- </tr> -->
+   <!-- <tr> -->
+       <!-- <td>Assis</td> -->
+       <!-- <td>São Paulo</td> -->
+    <!-- </tr> -->
+    <!-- <tr> -->
+       <!-- <td>Candido Mota</td> -->
+       <!-- <td>São Paulo</td> -->
+   <!-- </tr> -->
+   <!-- <tr> -->
+      <!-- <td>Londrina</td> -->
+      <!-- <td>Parana</td> -->
+   <!-- </tr> -->
+   <!-- <tr> -->
+       <!-- <td>Curitiba</td> -->
+       <!-- <td>Parana</td> -->
+    <!-- </tr> -->
+<!-- </table> -->
 
 </center>
 
